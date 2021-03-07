b30a5e2b2d1cdd99c310d421bc4093fbc29985cf
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-03-07 07:00:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,17 @@
 provider "azurerm" {
-  features {}
+  version         = "1.38.0"
+  subscription_id = var.sub
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "${var.location}"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "${var.prefix}storageacct"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["23.45.1.0/30"]
+resource "azurerm_resource_group" "TerraformPOCRG" {
+  name     = "TerraformPOCRG"
+  location = "eastus"
+  
+  tags = {
+    environment = "Dev"
   }
+
 }
